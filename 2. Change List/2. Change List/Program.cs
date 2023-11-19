aaa3e55229e26ffd5e9e65420649a06f2fc0d7de
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-11-19 14:57:49 +0200

    exercises (100/100)

---
+++ 2. Change List/2. Change List/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        string command;
+        while ((command = Console.ReadLine()) != "end")
+        {
+            string[] commandArgs = command.Split();
+            string action = commandArgs[0];
+
+            switch (action)
+            {
+                case "Delete":
+                    int elementToDelete = int.Parse(commandArgs[1]);
+                    DeleteElement(numbers, elementToDelete);
+                    break;
+
+                case "Insert":
+                    int elementToInsert = int.Parse(commandArgs[1]);
+                    int position = int.Parse(commandArgs[2]);
+                    InsertElement(numbers, elementToInsert, position);
+                    break;
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+
+    static void DeleteElement(List<int> numbers, int element)
+    {
+        numbers.RemoveAll(x => x == element);
+    }
+
+    static void InsertElement(List<int> numbers, int element, int position)
+    {
+        if (position >= 0 && position < numbers.Count)
+        {
+            numbers.Insert(position, element);
+        }
+    }
+}
